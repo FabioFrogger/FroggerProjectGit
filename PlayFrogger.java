b4594d813c77ec451415ed774cbc1c9e9fb5fb19
Stefano2398 <stefano.sironi@issgreppi.it>
2016-10-26 20:20:03 +0200

    Update PlayFrogger.java

--- PlayFrogger.java
+++ PlayFrogger.java
@@ -6,16 +6,24 @@ public class PlayFrogger extends JFrame {
 
     public static JLabel lab1;
     public static JLabel lab2;
+    public static JLabel lab3;
+    public static JLabel lab4;
 
     public PlayFrogger() {
         setSize(310,800);
         lab1 = new JLabel("LEVEL");
         lab1.setFont(new Font("Courier New", Font.ITALIC, 18));
         lab2=new JLabel("♥♥♥♥♥");
+        lab3 = new JLabel("SCORE");
+        lab3.setFont(new Font("Courier New", Font.TYPE1_FONT, 12));
+        lab4 = new JLabel("HIGHSCORE");
+        lab4.setFont(new Font("Courier New", Font.TYPE1_FONT, 12));
         GridLayout g=new GridLayout(2,2);
         getContentPane().setLayout(g);
         add(lab1);
         add(lab2);
+        add(lab3);
+        add(lab4);
         add(new FroggerComponent());
         setResizable(false);
         setLocationRelativeTo(null);
