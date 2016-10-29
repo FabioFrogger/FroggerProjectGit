4281e11aea0271378e855d6ce6973660979241b9
Stefano2398 <stefano.sironi@issgreppi.it>
2016-10-29 17:37:57 +0200

    Add files via upload

--- FroggerComponent.java
+++ FroggerComponent.java
@@ -1,7 +1,10 @@
+
 import java.awt.*;
 import java.util.prefs.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,20 +15,17 @@ public class FroggerComponent extends JComponent implements KeyListener, Runnabl
     private boolean upPressed, downPressed, leftPressed, rightPressed;
     public static Vector<FroggerLevel> levels = new Vector();
     private int level = 0;
-    //private JLabel l;
-    private int life=5;
-    private int score = 0;
-    private int highscore = readPreference();
+    private int life = 5;
+    private double score = 0;
+    private double highscore = readPreference();
     private static final String HIGHSCORE = "highscore1";
 
-    //creo playfrogger
-    //PlayFrogger pf = new PlayFrogger();
     public FroggerComponent() {
         super();
         levelInit();
         engine = new FroggerLevelEngine(levels.get(level));
         setPreferredSize(new Dimension(engine.WIDTH, engine.HEIGHT));
-        this.life=5;
+        this.life = 5;
         addKeyListener(this);
         Thread run = new Thread(this);
         run.start();
@@ -33,7 +33,6 @@ public class FroggerComponent extends JComponent implements KeyListener, Runnabl
 
     public void levelInit() {
 
-         
         levels.add(new FroggerLevel(
                 new int[]{1, 2, 1},
                 new String[]{"  YYYY         L  ", " BB      RR     ", "    RR    MM     "}));
@@ -73,7 +72,6 @@ public class FroggerComponent extends JComponent implements KeyListener, Runnabl
                 new int[]{2, 3, 1, 2, 3, 2},
                 new String[]{"LLL       LLL       ", "   RRRR     RRRR     ",
                     "RR  BB     LL  MM   ", "LLL    BB     RR ", "MMMMM     RBRBR    LLLL    ", "    BBB   MMMM      LLLLL"}));
-
 
         levels.add(new FroggerLevel(
                 new int[]{1, 3, 4, 3, 1, 2},
@@ -123,39 +121,43 @@ public class FroggerComponent extends JComponent implements KeyListener, Runnabl
 
         engine.update();
 
-        if(life==0)
-        {
-            PlayFrogger.lab2.setText("♥♥♥♥♥");
-            JOptionPane.showMessageDialog(this, "Game Over","No more lifes",JOptionPane.ERROR_MESSAGE);
-            downPressed=false;
-            leftPressed=false;
-            rightPressed=false;
-            upPressed=false;
-            level=0;
-            life=5;
+        if (life == 0) {
+            PlayFrogger.lab2.setText("");
+            PlayFrogger.prova.stop();
+            JOptionPane.showMessageDialog(this, "Game Over", "No more lifes", JOptionPane.ERROR_MESSAGE);
+            downPressed = false;
+            leftPressed = false;
+            rightPressed = false;
+            upPressed = false;
+            level = 0;
+            life = 5;
             engine = new FroggerLevelEngine(levels.get(level));
+            score = 0;
+            PlayFrogger.lab2.setText("♥♥♥♥♥");
+            PlayFrogger.start();
+
         }
         if (engine.getState() == FroggerState.WON) {
             level++;
-            score += 300;
+            score += round(PlayFrogger.scorePerSeconds(), 1);
             if (level >= levels.size()) {
                 JOptionPane.showMessageDialog(this, "You have beaten this game.");
+                score = 0;
+                PlayFrogger.lab2.setText("♥♥♥♥♥");
                 System.exit(0);
             }
             engine = new FroggerLevelEngine(levels.get(level));
-            if (life>=5){
-                String hearts="";
-                for(int i=0;i<life;++i)
-                {
-                    hearts=hearts+"♥";
+            if (life >= 5) {
+                String hearts = "";
+                for (int i = 0; i < life; ++i) {
+                    hearts = hearts + "♥";
                     PlayFrogger.lab2.setText(hearts);
                 }
-            } else{
+            } else {
                 ++life;
-                String hearts="";
-                for(int i=0;i<life;++i)
-                {
-                    hearts=hearts+"♥";
+                String hearts = "";
+                for (int i = 0; i < life; ++i) {
+                    hearts = hearts + "♥";
                     PlayFrogger.lab2.setText(hearts);
                 }
             }
@@ -164,82 +166,73 @@ public class FroggerComponent extends JComponent implements KeyListener, Runnabl
         if (engine.getState() == FroggerState.HIT) {
             engine = new FroggerLevelEngine(levels.get(level));
             --life;
-            String hearts="";
-            for(int i=0;i<life;++i)
-            {
-                hearts=hearts+"♥";
+            String hearts = "";
+            for (int i = 0; i < life; ++i) {
+                hearts = hearts + "♥";
                 PlayFrogger.lab2.setText(hearts);
             }
         }
-//                
-//                if(levels.get(level))
-//                {
-//                
-//                }
+
         if (level == 0) {
-//    JLabel l = new JLabel();
-//    l = pf.getLab();
-//    l.setText("1");
             checkHighscore();
             PlayFrogger.lab1.setText("LEVEL 1 - Easy");
-            PlayFrogger.lab3.setText("SCORE: " + Integer.toString(score));
+            PlayFrogger.lab3.setText("SCORE: " + Double.toString(score));
             //PlayFrogger.lab1.setHorizontalAlignment(JLabel.CENTER);
             //PlayFrogger.lab1.setLocation(200,200);
-            PlayFrogger.lab1.setLocation((369 - PlayFrogger.lab1.getWidth()) / 2, 5);
-            PlayFrogger.lab1.setForeground(Color.BLUE);
-//                            PlayFrogger.lab1.setFont(new Font("Courier New", Font.ITALIC, 12));
-            PlayFrogger.lab3.setLocation((369 - PlayFrogger.lab3.getWidth()), 56);
-            PlayFrogger.lab3.setForeground(Color.BLUE);
+//            PlayFrogger.lab1.setLocation((369 - PlayFrogger.lab1.getWidth()) / 2, 5);
+//            PlayFrogger.lab1.setForeground(Color.BLUE);
+//            
+//            PlayFrogger.lab3.setLocation((369 - PlayFrogger.lab3.getWidth()), 56);
+//            PlayFrogger.lab3.setForeground(Color.BLUE);
 
         } else if (level == 1) {
             checkHighscore();
             PlayFrogger.lab1.setText("LEVEL 2 - Easy");
-            PlayFrogger.lab3.setText("SCORE: " + Integer.toString(score));
+            PlayFrogger.lab3.setText("SCORE: " + Double.toString(score));
             //PlayFrogger.lab1.setHorizontalAlignment(JLabel.CENTER);
             //PlayFrogger.lab1.setLocation(200,200);
-            PlayFrogger.lab1.setLocation((369 - PlayFrogger.lab1.getWidth()) / 2, 5);
-            PlayFrogger.lab1.setForeground(Color.RED);
-            PlayFrogger.lab3.setLocation((369 - PlayFrogger.lab3.getWidth()), 56);
-            PlayFrogger.lab3.setForeground(Color.BLUE);
-            
+//            PlayFrogger.lab1.setLocation((369 - PlayFrogger.lab1.getWidth()) / 2, 5);
+//            PlayFrogger.lab1.setForeground(Color.RED);
+//            PlayFrogger.lab3.setLocation((369 - PlayFrogger.lab3.getWidth()), 56);
+//            PlayFrogger.lab3.setForeground(Color.BLUE);
 
         } else if (level == 2) {
             checkHighscore();
             PlayFrogger.lab1.setText("LEVEL 3 - Medium");
-            PlayFrogger.lab3.setText("SCORE: " + Integer.toString(score));
+            PlayFrogger.lab3.setText("SCORE: " + Double.toString(score));
             //PlayFrogger.lab1.setHorizontalAlignment(JLabel.CENTER);
             //PlayFrogger.lab1.setLocation(200,200);
-            PlayFrogger.lab1.setLocation((369 - PlayFrogger.lab1.getWidth()) / 2, 5);
-            PlayFrogger.lab1.setForeground(Color.GREEN);
-            PlayFrogger.lab3.setLocation((369 - PlayFrogger.lab3.getWidth()), 56);
-            PlayFrogger.lab3.setForeground(Color.BLUE);
+//            PlayFrogger.lab1.setLocation((369 - PlayFrogger.lab1.getWidth()) / 2, 5);
+//            PlayFrogger.lab1.setForeground(Color.GREEN);
+//            PlayFrogger.lab3.setLocation((369 - PlayFrogger.lab3.getWidth()), 56);
+//            PlayFrogger.lab3.setForeground(Color.BLUE);
 
         } else if (level == 3) {
             checkHighscore();
             PlayFrogger.lab1.setText("LEVEL 4 - Medium");
-            PlayFrogger.lab3.setText("SCORE: " + Integer.toString(score));
+            PlayFrogger.lab3.setText("SCORE: " + Double.toString(score));
             //PlayFrogger.lab1.setHorizontalAlignment(JLabel.CENTER);
             //PlayFrogger.lab1.setLocation(200,200);
-            PlayFrogger.lab1.setLocation((369 - PlayFrogger.lab1.getWidth()) / 2, 5);
-            PlayFrogger.lab1.setForeground(Color.MAGENTA);
-            PlayFrogger.lab3.setLocation((369 - PlayFrogger.lab3.getWidth()), 56);
-            PlayFrogger.lab3.setForeground(Color.BLUE);
+//            PlayFrogger.lab1.setLocation((369 - PlayFrogger.lab1.getWidth()) / 2, 5);
+//            PlayFrogger.lab1.setForeground(Color.MAGENTA);
+//            PlayFrogger.lab3.setLocation((369 - PlayFrogger.lab3.getWidth()), 56);
+//            PlayFrogger.lab3.setForeground(Color.BLUE);
 
         } else if (level == 4) {
             checkHighscore();
             PlayFrogger.lab1.setText("LEVEL 5 - Medium");
-            PlayFrogger.lab3.setText("SCORE: " + Integer.toString(score));
+            PlayFrogger.lab3.setText("SCORE: " + Double.toString(score));
             //PlayFrogger.lab1.setHorizontalAlignment(JLabel.CENTER);
             //PlayFrogger.lab1.setLocation(200,200);
-            PlayFrogger.lab1.setLocation((369 - PlayFrogger.lab1.getWidth()) / 2, 5);
-            PlayFrogger.lab1.setForeground(Color.PINK);
-            PlayFrogger.lab3.setLocation((369 - PlayFrogger.lab3.getWidth()), 56);
-            PlayFrogger.lab3.setForeground(Color.BLUE);
+//            PlayFrogger.lab1.setLocation((369 - PlayFrogger.lab1.getWidth()) / 2, 5);
+//            PlayFrogger.lab1.setForeground(Color.PINK);
+//            PlayFrogger.lab3.setLocation((369 - PlayFrogger.lab3.getWidth()), 56);
+//            PlayFrogger.lab3.setForeground(Color.BLUE);
 
         } else if (level == 5) {
             checkHighscore();
             PlayFrogger.lab1.setText("LEVEL 6 - Difficult");
-            PlayFrogger.lab3.setText("SCORE: " + Integer.toString(score));
+            PlayFrogger.lab3.setText("SCORE: " + Double.toString(score));
             //PlayFrogger.lab1.setHorizontalAlignment(JLabel.CENTER);
             //PlayFrogger.lab1.setLocation(200,200);
             PlayFrogger.lab1.setLocation((369 - PlayFrogger.lab1.getWidth()) / 2, 5);
@@ -247,41 +240,41 @@ public class FroggerComponent extends JComponent implements KeyListener, Runnabl
             PlayFrogger.lab3.setLocation((369 - PlayFrogger.lab3.getWidth()), 56);
             PlayFrogger.lab3.setForeground(Color.BLUE);
 
-        }else if (level == 6) {
+        } else if (level == 6) {
             checkHighscore();
             PlayFrogger.lab1.setText("LEVEL 6 - Difficult");
-            PlayFrogger.lab3.setText("SCORE: " + Integer.toString(score));
+            PlayFrogger.lab3.setText("SCORE: " + Double.toString(score));
             //PlayFrogger.lab1.setHorizontalAlignment(JLabel.CENTER);
             //PlayFrogger.lab1.setLocation(200,200);
-            PlayFrogger.lab1.setLocation((369 - PlayFrogger.lab1.getWidth()) / 2, 5);
-            PlayFrogger.lab1.setForeground(Color.GRAY);
-            PlayFrogger.lab3.setLocation((369 - PlayFrogger.lab3.getWidth()), 56);
-            PlayFrogger.lab3.setForeground(Color.BLUE);
+//            PlayFrogger.lab1.setLocation((369 - PlayFrogger.lab1.getWidth()) / 2, 5);
+//            PlayFrogger.lab1.setForeground(Color.GRAY);
+//            PlayFrogger.lab3.setLocation((369 - PlayFrogger.lab3.getWidth()), 56);
+//            PlayFrogger.lab3.setForeground(Color.BLUE);
 
-        }else if (level == 7) {
+        } else if (level == 7) {
             checkHighscore();
             PlayFrogger.lab1.setText("LEVEL 7 - Difficult");
-            PlayFrogger.lab3.setText("SCORE: " + Integer.toString(score));
+            PlayFrogger.lab3.setText("SCORE: " + Double.toString(score));
             //PlayFrogger.lab1.setHorizontalAlignment(JLabel.CENTER);
             //PlayFrogger.lab1.setLocation(200,200);
-            PlayFrogger.lab1.setLocation((369 - PlayFrogger.lab1.getWidth()) / 2, 5);
-            PlayFrogger.lab1.setForeground(Color.GRAY);
-            PlayFrogger.lab3.setLocation((369 - PlayFrogger.lab3.getWidth()), 56);
-            PlayFrogger.lab3.setForeground(Color.BLUE);
+//            PlayFrogger.lab1.setLocation((369 - PlayFrogger.lab1.getWidth()) / 2, 5);
+//            PlayFrogger.lab1.setForeground(Color.GRAY);
+//            PlayFrogger.lab3.setLocation((369 - PlayFrogger.lab3.getWidth()), 56);
+//            PlayFrogger.lab3.setForeground(Color.BLUE);
 
-        }else if (level == 8) {
+        } else if (level == 8) {
             checkHighscore();
             PlayFrogger.lab1.setText("LEVEL 9 - Impossible");
-            PlayFrogger.lab3.setText("SCORE: " + Integer.toString(score));
+            PlayFrogger.lab3.setText("SCORE: " + Double.toString(score));
             //PlayFrogger.lab1.setHorizontalAlignment(JLabel.CENTER);
             //PlayFrogger.lab1.setLocation(200,200);
-            PlayFrogger.lab1.setLocation((369 - PlayFrogger.lab1.getWidth()) / 2, 5);
-            PlayFrogger.lab1.setForeground(Color.GRAY);
-            PlayFrogger.lab3.setLocation((369 - PlayFrogger.lab3.getWidth()), 56);
-            PlayFrogger.lab3.setForeground(Color.BLUE);
+//            PlayFrogger.lab1.setLocation((369 - PlayFrogger.lab1.getWidth()) / 2, 5);
+//            PlayFrogger.lab1.setForeground(Color.GRAY);
+//            PlayFrogger.lab3.setLocation((369 - PlayFrogger.lab3.getWidth()), 56);
+//            PlayFrogger.lab3.setForeground(Color.BLUE);
 
         }
-          
+
     }
 
     public void keyPressed(KeyEvent ke) {
@@ -315,29 +308,40 @@ public class FroggerComponent extends JComponent implements KeyListener, Runnabl
 
     public void keyTyped(KeyEvent ke) {
     }
-    
+
     private void checkHighscore() {
         if (score > highscore) {
             highscore = score;
 
         }
 
-        PlayFrogger.lab4.setText("HIGHSCORE: " + Integer.toString(highscore));
-        PlayFrogger.lab4.setLocation((369 - PlayFrogger.lab4.getWidth()), 46);
-        PlayFrogger.lab4.setForeground(Color.RED);
+        PlayFrogger.lab4.setText("HIGHSCORE: " + Double.toString(highscore));
+//        PlayFrogger.lab4.setLocation((369 - PlayFrogger.lab4.getWidth()), 46);
+//        PlayFrogger.lab4.setForeground(Color.RED);
         savePreference(highscore);
 
     }
 
-    public void savePreference(int highscore) {
+    public void savePreference(double highscore) {
         Preferences prefs = Preferences.userNodeForPackage(FroggerComponent.class);
 
-        prefs.putInt(HIGHSCORE, highscore);
+        prefs.putDouble(HIGHSCORE, highscore);
     }
 
-    public int readPreference() {
+    public double readPreference() {
         Preferences prefs = Preferences.userNodeForPackage(FroggerComponent.class);
 
-        return prefs.getInt(HIGHSCORE, 0);
+        return prefs.getDouble(HIGHSCORE, 0);
     }
+
+    public static double round(double value, int places) {
+        if (places < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
 }
--- PlayFrogger.java
+++ PlayFrogger.java
@@ -1,6 +1,10 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PlayFrogger extends JFrame {
 
@@ -8,34 +12,99 @@ public class PlayFrogger extends JFrame {
     public static JLabel lab2;
     public static JLabel lab3;
     public static JLabel lab4;
+    public static JLabel labTime;
+    private static long startTime;
+    public static ThreadTime prova;
+    private static int seconds;
+    private static int minutes;
+    private static int hours;
 
     public PlayFrogger() {
-        setSize(310,800);
+        setSize(310, 800);
         lab1 = new JLabel("LEVEL");
         lab1.setFont(new Font("Courier New", Font.ITALIC, 18));
-        lab2=new JLabel("♥♥♥♥♥");
+        lab2 = new JLabel("♥♥♥♥♥");
         lab3 = new JLabel("SCORE");
         lab3.setFont(new Font("Courier New", Font.TYPE1_FONT, 12));
         lab4 = new JLabel("HIGHSCORE");
         lab4.setFont(new Font("Courier New", Font.TYPE1_FONT, 12));
-        GridLayout g=new GridLayout(2,2);
+        PlayFrogger.lab1.setLocation((369 - PlayFrogger.lab1.getWidth()) / 2, 5);
+        PlayFrogger.lab1.setForeground(Color.BLUE);
+
+        PlayFrogger.lab3.setLocation((369 - PlayFrogger.lab3.getWidth()), 56);
+        PlayFrogger.lab3.setForeground(Color.BLUE);
+
+        PlayFrogger.lab4.setLocation((369 - PlayFrogger.lab4.getWidth()), 46);
+        PlayFrogger.lab4.setForeground(Color.RED);
+        labTime = new JLabel("0:00:00");
+        labTime.setFont(new Font("Courier New", Font.TYPE1_FONT, 12));
+        PlayFrogger.labTime.setLocation((369 - PlayFrogger.lab4.getWidth()), 76);
+        PlayFrogger.labTime.setForeground(Color.DARK_GRAY);
+//        label.setHorizontalAlignment(JLabel.CENTER);
+
+//        panelButtons = new JPanel(new GridLayout(1, 2));
+//        GridLayout g = new GridLayout(2, 2);
+//        getContentPane().setLayout(g);
+        GridLayout g = new GridLayout(2, 2);
         getContentPane().setLayout(g);
+
         add(lab1);
         add(lab2);
         add(lab3);
         add(lab4);
+        add(labTime);
+
+        start();
+
         add(new FroggerComponent());
+
         setResizable(false);
         setLocationRelativeTo(null);
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-        
-         //lab1.setLocation(75,40);
+
+        //lab1.setLocation(75,40);
+    }
+
+    public static class ThreadTime extends Thread {
+
+        @Override
+        public void run() {
+
+            while (true) {
+                long diffTime = System.currentTimeMillis() - startTime;
+//                int decSeconds = (int) (diffTime % 1000 / 100);
+                seconds = (int) (diffTime / 1000 % 60);
+                minutes = (int) (diffTime / 60000 % 60);
+                hours = (int) (diffTime / 3600000);
+                String s = String.format("%d:%02d:%02d", hours, minutes, seconds);
+
+                labTime.setText(s);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(PlayFrogger.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+
     }
 
     public static void main(String[] args) {
         new PlayFrogger();
     }
-     
+
+    public static void start() {
+        startTime = System.currentTimeMillis();
+        prova = new ThreadTime();
+        prova.start();
+    }
+
+    public static double scorePerSeconds() {
+
+        return (double)(1000.0 / (seconds + minutes * 60 + hours * 3600));
+
+    }
+
 }
